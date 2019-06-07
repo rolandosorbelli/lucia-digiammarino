ea18c352c06a433ee14c2996139fcfe54593dd0f
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-06-07 16:57:05 +0100

    Added NewsHeader

---
+++ src/components/NewsHeader/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+class NewsHeader extends React.Component {
+
+  render(){
+
+    return(
+      <div className="newsHeader">
+        <h1 className="newsHeader__title">Blog Posts</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+          <polygon fill="white" points="0,40 100,100 0,100"/>
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default NewsHeader
