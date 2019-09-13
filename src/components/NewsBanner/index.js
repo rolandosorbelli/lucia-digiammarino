c63bbe856151c4f3862f279b3b4fee3865b1a4d2
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-09-13 16:08:15 +0100

    Fixed Stickyfill issue with Gatsby

--- src/components/NewsBanner/index.js
+++ src/components/NewsBanner/index.js
@@ -1,36 +1,35 @@
 import React from "react"
-import Stickyfill from 'stickyfilljs'
+import Stickyfill from "stickyfilljs"
 
-import arrow from '../../images/down-arrow.svg'
+import arrow from "../../images/down-arrow.svg"
 
-import jump from 'jump.js'
+import jump from "jump.js"
 
 class NewsBanner extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
 
-    this.state = {};
+    this.state = {}
   }
 
-  render(){
+  componentDidMount() {
+    if (typeof window !== "undefined") {
+      const Stickyfill = require("stickyfilljs")
+    }
+  }
 
-    const banner = document.querySelectorAll('.newsbanner')
+  render() {
+    const banner = document.querySelectorAll(".newsbanner")
     Stickyfill.add(banner)
 
-    return(
-      <button
-        className="newsbanner__link"
-        onClick={()=> jump('.newsHeader')}>
-          <div className="newsbanner">
-            <h2>Blog Posts</h2>
-            <img
-              className="newsbanner__arrow"
-              src={arrow}
-              alt=""
-            />
-          </div>
+    return (
+      <button className="newsbanner__link" onClick={() => jump(".newsHeader")}>
+        <div className="newsbanner">
+          <h2>Blog Posts</h2>
+          <img className="newsbanner__arrow" src={arrow} alt="" />
+        </div>
       </button>
-    );
+    )
   }
 }
 
