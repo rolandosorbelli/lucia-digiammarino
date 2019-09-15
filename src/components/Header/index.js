a998bad759bf922de4d45d24592e4212687e72fd
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-09-15 19:31:23 +0100

    Small tweak

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,46 +3,45 @@ import React from "react"
 import logo from "../../images/logo.svg"
 
 class Header extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props)
 
-    this.state = {};
+    this.state = {}
   }
 
-  render(){
-
+  render() {
     return (
       <header>
         <div className="header__items">
-          <a
-            className="header__items--left"
-            href="/">
+          <a className="header__items--left" href="/">
             <img
               className="header__items--logo"
               src={logo}
-              alt='Lucia Di Giammarino Logo'
+              alt="Lucia Di Giammarino Logo"
             />
           </a>
           <div className="header__items--right">
             <div className="header__items--right__text">
-              <h1>Lucia Di Giammarino</h1>
+              <h1>Lucia</h1>
+              <h1>Di Giammarino</h1>
               <h2>2D artist</h2>
             </div>
           </div>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-          <polygon fill="white" points="0,40 100,100 0,100"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="white" points="0,40 100,100 0,100" />
         </svg>
       </header>
     )
   }
 }
 
-Header.propTypes = {
-}
+Header.propTypes = {}
 
-Header.defaultProps = {
-
-}
+Header.defaultProps = {}
 
 export default Header
