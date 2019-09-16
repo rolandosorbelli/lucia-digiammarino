e5a70ca512478c03a5143d3114603b97d588296e
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-09-16 23:05:11 +0100

    Added Google Fonts package

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lucia Di Giammarino`,
+    description: `Lucia Di Giammarino | Portfolio`,
+    author: `Rolando Sorbelli`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,6 +15,12 @@ module.exports = {
       options: {
         spaceId: `tkb6gelx5w7j`,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`lato`],
       },
     },
     {
