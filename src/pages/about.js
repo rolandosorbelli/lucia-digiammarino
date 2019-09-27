370ca40a38fe4c56706c1e6ea36a001ff8f08447
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-09-27 15:19:17 +0100

    Added menu in the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import Header from "../components/Header"
 import AboutContent from "../components/AboutContent"
+import Menu from "../components/Menu"
 
 const About = () => {
   return (
     <div className="main">
       <Header />
+      <Menu />
       <AboutContent />
     </div>
   )
