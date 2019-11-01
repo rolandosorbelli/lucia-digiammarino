4bea44bf544a0948626d64958908855f2d57bcd2
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-11-01 13:18:19 +0000

    Added previous and next posts

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -29,18 +29,39 @@ export const query = graphql`
         }
       }
     }
-    allContentfulBlogPost {
+    allContentfulBlogPost(sort: { fields: createdAt, order: DESC }) {
       edges {
         node {
-          id
+          slug
+          title
         }
       }
     }
   }
 `
 
-const blogPost = props => {
-  console.log(props.data)
+const BlogPost = props => {
+  // console.log(props.data.allContentfulBlogPost)
+  let currentPostId = props.data.contentfulBlogPost.slug
+  let allPosts = props.data.allContentfulBlogPost.edges
+  let allSlugs = []
+  Object.values(allPosts).filter(value =>
+    allSlugs.push(value.node.slug, value.node.title)
+  )
+  console.log(allSlugs)
+  let index = allSlugs.indexOf(currentPostId)
+  let prevItem
+  let prevTitle
+  let nextItem
+  let nextTitle
+  if (index >= 0 && index < allSlugs.length - 1) {
+    prevItem = allSlugs[index + 2]
+    prevTitle = allSlugs[index + 3]
+    nextItem = allSlugs[index - 2]
+    nextTitle = allSlugs[index - 1]
+  }
+  console.log(prevItem, prevTitle, "PREV")
+  console.log(nextItem, nextTitle, "NEXT")
 
   return (
     <Layout>
@@ -59,6 +80,7 @@ const blogPost = props => {
           <h1 className="blogpost__title">
             {props.data.contentfulBlogPost.title}
           </h1>
+
           <p className="blogpost__date">
             {props.data.contentfulBlogPost.createdAt}
           </p>
@@ -68,10 +90,20 @@ const blogPost = props => {
               __html: `${props.data.contentfulBlogPost.content.childMarkdownRemark.html}`,
             }}
           ></div>
+          <div className="blogpost__promoWrapper">
+            <div className="blogpost__promoPrev">
+              <p>PREVIOUS</p>
+              <a href={`/blog/${prevItem}`}>{prevTitle}</a>
+            </div>
+            <div className="blogpost__promoNext">
+              <p>NEXT</p>
+              <a href={`/blog/${nextItem}`}>{nextTitle}</a>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
   )
 }
 
-export default blogPost
+export default BlogPost
