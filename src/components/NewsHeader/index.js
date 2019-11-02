a3f254dc4363ca76405a3fd4497816b5c5141f1e
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-11-02 13:53:27 +0000

    Cleaned and remove some style

--- src/components/NewsHeader/index.js
+++ src/components/NewsHeader/index.js
@@ -1,17 +1,19 @@
 import React from "react"
 
 class NewsHeader extends React.Component {
-
-  render(){
-
-    return(
+  render() {
+    return (
       <div className="newsHeader">
-        <h1 className="newsHeader__title">Blog Posts</h1>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-          <polygon fill="white" points="0,40 100,100 0,100"/>
+        <h1 className="newsHeader__title">What's new?</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="white" points="0,40 100,100 0,100" />
         </svg>
       </div>
-    );
+    )
   }
 }
 
