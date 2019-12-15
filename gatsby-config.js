24e60c1f1aa68f3408c0c66e819a719d50c87f0f
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-12-15 22:00:33 +0000

    Edited metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Lucia Di Giammarino`,
-    description: `Lucia Di Giammarino | Portfolio`,
+    description: `| Portfolio`,
     author: `Rolando Sorbelli`,
   },
   plugins: [
