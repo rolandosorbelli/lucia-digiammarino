bdac4778f2adf79b0228f0d1770f57b3c6da78c1
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2019-12-16 08:30:32 +0000

    Added Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,12 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-154660451-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
